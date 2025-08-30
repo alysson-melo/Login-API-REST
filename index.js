aead77b2385251bcda3cdd291692ccdce49ba743
Alysson Melo <alyssonarturmelo@gmail.com>
2025-08-30 15:51:26 -0300

    Configurações iniciais

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Erro ao subir a aplicação");
+        return;
+    }
+    console.log("Aplicação rodando");
+});
